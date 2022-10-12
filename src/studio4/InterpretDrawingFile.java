0e874f18a5dcaec20b811cd570d6a784207671c6
jchen38 <jchen38@10.232.185.246>
2022-10-12 15:48:28 -0500

    InterepretDrawing

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,7 @@
 package studio4;
 
 import java.io.File;
+import java.awt.Color;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -19,6 +20,69 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
 		
+
+
+		System.out.println(shape);
+		System.out.println(r);
+		System.out.println(g);
+		System.out.println(b);
+		System.out.println(isFilled);
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		System.out.println(p4);
+
+		StdDraw.setPenColor(r,g,b);
+
+		if (shape.equals("rectangle")) {
+			if (isFilled == true) {
+				StdDraw.filledRectangle(p1, p2, p3, p4);
+			}
+			else {
+				StdDraw.rectangle(p1, p2, p3, p4);
+			}
+		}
+
+
+
+		if (shape.equals("ellipse")) {
+			if(isFilled == true) {
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}
+			else {
+				StdDraw.ellipse(p1, p2, p3, p4);
+			}
+
+		if (shape.equals("triangle")) {
+			double p5 = in.nextDouble();
+			double p6 = in.nextDouble();
+				StdDraw.line(p1, p2, p3, p4);
+				StdDraw.line(p3, p4, p5, p6);
+				StdDraw.line(p5, p6, p1, p2);
+			}
+			
+		}
+
+
+
+
+
+
+
+
+
+		}
+
 	}
-}
+
