cbe3848a64ae1d5f1bc27f036cf2594da208c912
jchen38 <jchen38@10.232.185.246>
2022-10-12 15:47:51 -0500

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,22 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.rectangle(0.5, 0.5, 0.3, 0.2);
+		StdDraw.ellipse(0.28, 0.6, 0.05, 0.05);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.01);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
